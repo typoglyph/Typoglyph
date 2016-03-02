0ae8cbd85afc626736a5b7c858d41b0b43a22636
jakemarsden <jakemarsdenjm@gmail.com>
2016-03-03 01:06:43 +1300

    Added favicon

--- src/info.php
+++ src/info.php
@@ -3,7 +3,9 @@
 <head>
 	<meta content="text/html;charset=utf-8" http-equiv="Content-Type">
 	<meta content="utf-8" http-equiv="encoding">
+	
 	<title>Typoglyph</title>
+	<link rel="icon" type="image/ico" href="images/favicon.ico">
 	
 	<link rel="stylesheet" type="text/css" href="styles/common.css">
 </head>
