4f87af35b23d20cb81811e81ffb5a733a0218cd2
jakemarsden <jakemarsdenjm@gmail.com>
2016-03-03 01:06:00 +1300

    Refactored css to own files

--- src/info.php
+++ src/info.php
@@ -4,6 +4,8 @@
 	<meta content="text/html;charset=utf-8" http-equiv="Content-Type">
 	<meta content="utf-8" http-equiv="encoding">
 	<title>Typoglyph</title>
+	
+	<link rel="stylesheet" type="text/css" href="styles/common.css">
 </head>
 <body>
     <?php
