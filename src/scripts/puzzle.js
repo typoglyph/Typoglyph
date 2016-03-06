665a3d1088d127ae17e0203169fdfedc06bbef24
jakemarsden <jakemarsdenjm@gmail.com>
2016-03-07 00:40:45 +1300

    .equals() and .toString() methods for puzzle classes

--- src/scripts/puzzle.js
+++ src/scripts/puzzle.js
@@ -63,6 +63,59 @@ typoglyph.puzzle.Puzzle = function(sentence, options, gaps) {
 		}
 		return null;
 	}
+	
+	/**
+	 * @return {String}
+	 */
+	this.toString = function() {
+		var str = "";
+		var sentence = _sentence;
+		var gaps = _gaps;
+		for (i = 0; i <= sentence.length; i++) {
+			for (j = 0; j < gaps.length; j++) {
+				// Is the gap at the current position?
+				if (gaps[j].getPosition() === i) {
+					str += gaps[j];
+					break;
+				}
+			}
+			str += sentence.charAt(i);
+		}
+		return str;
+	}
+	
+	/**
+	 * @param {Puzzle} other
+	 * @return {boolean}
+	 */
+	this.equals = function(other) {
+		/**
+		 * @param {Array<?>} a
+		 * @param {Array<?>} b
+		 * @return {boolean}
+		 */
+		function arrayEquals(a, b) {
+			if (a === null || b === null)
+				return (a === b);
+			if (a.length !== b.length)
+				return false;
+			for (i = 0; i < a.length; i++) {
+				if (a[i] === null) {
+					if (b[i] !== null) {
+						return false;
+					}
+				} else if (!a[i].equals(b[i])) {
+					return false;
+				}
+			}
+			return true;
+		}
+		
+		return other !== null
+			&& _sentence === other.getSentence()
+			&& arrayEquals(_options, other.getOptions())
+			&& arrayEquals(_gaps, other.getGaps());
+	}
 }
 
 
@@ -105,6 +158,13 @@ typoglyph.puzzle.Gap = function(position, solution) {
 	}
 	
 	/**
+	 * @return {boolean}
+	 */
+	this.hasCurrentChoice = function() {
+		return (_currentChoice !== null);
+	}
+	
+	/**
 	 * @return {Option}
 	 */
 	this.getCurrentChoice = function() {
@@ -116,6 +176,24 @@ typoglyph.puzzle.Gap = function(position, solution) {
 	 */
 	this.setCurrentChoice = function(newChoice) {
 		_currentChoice = newChoice;
+	}
+	
+	/**
+	 * @return {String}
+	 */
+	this.toString = function() {
+		return "{" + _solution + "}";
+	}
+	
+	/**
+	 * @param {Gap} other
+	 * @return {boolean}
+	 */
+	this.equals = function(other) {
+		return other !== null
+			&& _position === other.getPosition()
+			&& (_solution === null) ? (other.getSolution() === null) : _solution.equals(other.getSolution())
+			&& (_currentChoice === null) ? (other.getCurrentChoice() === null) : _currentChoice.equals(other.getCurrentChoice());
 	}
 }
 /** @private */ typoglyph.puzzle.Gap._nextId = 0;
@@ -142,6 +220,22 @@ typoglyph.puzzle.Option = function(value) {
 	 */
 	this.getValue = function() {
 		return _value;
+	}
+	
+	/**
+	 * @return {String}
+	 */
+	this.toString = function() {
+		return _value;
+	}
+	
+	/**
+	 * @param {Option} other
+	 * @return {boolean}
+	 */
+	this.equals = function(other) {
+		return other !== null
+			&& _value === other.getValue();
 	}
 }
 /** @private */ typoglyph.puzzle.Option._nextId = 0;
