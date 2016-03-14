35cb11ee49c6af1214b1f5fc0326344976474632
jakemarsden <jakemarsdenjm@gmail.com>
2016-03-15 03:39:17 +1300

    - Added new test data - Changed set size to 5 puzzles

--- env/mysql_generate_test_data.sql
+++ env/mysql_generate_test_data.sql
@@ -3,10 +3,16 @@ USE `typoglyph`;
 TRUNCATE `puzzles`;
 INSERT INTO `puzzles` (`options`, `sentence`) VALUES
 	-- Backslashes and single quotes must be escaped with a preceding backslash if you want them to appear literally in the table
-	(',;#!',	'Hello world{!}'),
-	('.,?#',	'This is a test sentence{.} Can you work out which punctuation to use{?}'),
+	('!',		'Hello{} world{!}'),
+	('.,?',		'This{} is{} a{} test{} sentence{.} Can{} you{} work{} out{} which{} punctuation{} to{} use{?}'),
+	('.,?',		'Hello{,} my{} name{} is{} Jake{.} How{} are{} you{} today{?}'),
+	(',.;-:',	'The{} dog{} ran{} over{} the{} car{,} but{} the{} driver{} was{} able{} to{} walk{} away{} uninjured{.}'),
+	('.,?',		'Have{} you{} tried{} turning{} it{} off{} and{} on{} again{?}'),
+	('.,?',		'Are{} you{} sure{} it\'s{} plugged{} in{?}'),
+	
+	
+	-- Specifically for testing edge cases
+	('',		'What{} if{} no{} punctuation{} is{} needed{}'),
+	('',		'What if there aren\'t even any gaps'),
 	('{}',		'{\\{} Example wrapped in curly brackets to ensure escaping works right {\\}}'),
-	('\\',		'{\\\\} Example wrapped in backslashes to ensure escaping works right {\\\\}'),
-	('',		'What{} if{} no{} punctuation{} is{} needed?'),
-	('',		'What if there aren\'t even any gaps?'),
-	('.,?#',	'Hello{,} my name is Jake{.} How are you today{?}');
+	('\\',		'{\\\\} Example wrapped in backslashes to ensure escaping works right {\\\\}');
