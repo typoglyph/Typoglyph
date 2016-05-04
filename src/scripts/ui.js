bbc9a7f17a2eba95ff7f8b52c73b319ec1f06a38
jakemarsden <jakemarsdenjm@gmail.com>
2016-05-04 23:16:54 +1200

    Random completion sound now played alongside completion graphics. - IMPORTANT: Sounds are placeholders for testing purposes only - Sound from http://www.grsites.com/archive/sounds/

--- src/scripts/ui.js
+++ src/scripts/ui.js
@@ -358,3 +358,48 @@ typoglyph.ui.CompletionGraphicDrawer = Objects.subclass(typoglyph.ui.Drawer, {
 		util.removeAllChildren(p);
 	}
 });
+
+
+typoglyph.ui.CompletionSoundPlayer = {
+	/**
+	 * @param {Array<String>} correctSounds
+	 * @param {Array<String>} incorrectSounds
+	 * @constructor
+	 */
+	create: function(correctSounds, incorrectSounds) {
+		var self = Objects.subclass(this);
+		self.correctSounds = correctSounds;
+		self.incorrectSounds = incorrectSounds;
+		
+		// May need to be tweaked depending on the sounds being played
+		self.volume = 1.0;
+		return self;
+	},
+	
+	/**
+	 * @param {boolean} correct
+	 */
+	playSound: function(correct) {
+		var util = typoglyph.util;
+		var sound = util.randomElement(correct ? this.correctSounds : this.incorrectSounds);
+		this.initPlayer([sound], this.volume).play();
+	},
+	
+	/**
+	 * @private
+	 */
+	initPlayer: function(urls, volumne) {
+		return new Howl({
+			urls: urls,
+			volume: this.volume,
+			autoplay: false,
+			loop: false,
+			
+			onload:			function() { console.debug("Howl.onLoad: " + urls); },
+			onloaderror:	function() { console.warn("Howl.onLoadError: " + urls); },
+			onplay:			function() { console.debug("Howl.onPlay: " + urls); },
+			onpause:		function() { console.debug("Howl.onPause: " + urls); },
+			onend:			function() { console.debug("Howl.onEnd: " + urls); }
+		});
+	}
+}
