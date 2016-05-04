1dbe867c6c624a654657cba250fbe6cf5b369cd5
jakemarsden <jakemarsdenjm@gmail.com>
2016-05-05 01:08:55 +1200

    Ability to mute sound effects

--- src/scripts/ui.js
+++ src/scripts/ui.js
@@ -360,6 +360,75 @@ typoglyph.ui.CompletionGraphicDrawer = Objects.subclass(typoglyph.ui.Drawer, {
 });
 
 
+typoglyph.ui.ToggleButtonDrawer = Objects.subclass(typoglyph.ui.Drawer, {
+	/**
+	 * @param {string} onImage Image used to represent the button when in the "on" state
+	 * @param {string} offImage Image used to represent the button when in the "off" state
+	 * @constructor
+	 */
+	create: function(onImage, offImage) {
+		var self = typoglyph.ui.Drawer.create.call(this);
+		self.onImage = onImage;
+		self.offImage = offImage;
+		self.enabled = false;
+		self.stateListener = null;
+		return self;
+	},
+	
+	
+	/**
+	 * @param {function(boolean, boolean)}
+	 */
+	setOnStateChangeListener: function(listener) {
+		this.stateListener = listener;
+	},
+	
+	
+	/**
+	 * @return {boolean}
+	 */
+	isEnabled: function() {
+		return this.enabled;
+	},
+	
+	/**
+	 * @param {boolean} newValue
+	 */
+	setEnabled: function(newValue) {
+		var oldValue = this.enabled;
+		
+		this.enabled = newValue;
+		if (this.stateListener !== null) {
+			this.stateListener(oldValue, newValue);
+		}
+	},
+	
+	toggle: function() {
+		this.setEnabled(!this.isEnabled());
+	},
+	
+	
+	/**
+	 * @param {Object} obj Unused
+	 * @return {HTMLElement}
+	 * @override
+	 */
+	createRootElement: function(obj) {
+		var e = this.newElement("img", "", "toggleButton");
+		return e;
+	},
+	
+	/**
+	 * @param {HTMLElement} p
+	 * @param {Object} obj Unused
+	 * @override
+	 */
+	drawInto: function(p, obj) {
+		p.src = this.isEnabled() ? this.onImage : this.offImage;
+	}
+});
+
+
 typoglyph.ui.CompletionSoundPlayer = {
 	/**
 	 * @param {Array<String>} correctSounds
