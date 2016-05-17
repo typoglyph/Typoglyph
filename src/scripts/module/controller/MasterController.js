c84ffb13af51fa586c3b49bcea1277b9aa4e9a4d
jakemarsden <jakemarsdenjm@gmail.com>
2016-05-18 03:05:43 +1200

    Improved documentation

--- src/scripts/module/controller/MasterController.js
+++ src/scripts/module/controller/MasterController.js
@@ -1,4 +1,27 @@
 /**
+ * A set of content controllers. The general idea is to have a layout something like:
+ * <code>
+ * <div id="masterContent">
+ *     <div id="content1">...</div>
+ *     <div id="content2">...</div>
+ *     ...
+ * </div>
+ * </code>
+ * 
+ * And define a controller subclass for each content controller. Then in your main script for the
+ * page:
+ * <code>
+ * var content1 = ContentController1.create(#content1);
+ * var content2 = ContentController2.create(#content2);
+ * var masterController = MasterController.create([content1, content2]);
+ * </code>
+ * 
+ * You can now switch between the shown content using:
+ * <code>
+ * masterController.show(content1);
+ * masterController.show(content2);
+ * </code>
+ * 
  * @author jakemarsden
  */
 define(["util/Objects"], function(Objects) {
