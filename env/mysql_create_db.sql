7f4af240fbe8fbdbb8d474ffe5be7f8efee32638
jakemarsden <jakemarsdenjm@gmail.com>
2016-05-30 01:19:38 +1200

    Update backend to store puzzles in JSON format

--- env/mysql_create_db.sql
+++ env/mysql_create_db.sql
@@ -1,21 +1,19 @@
+-- Don't drop the database as we'll lose all user privilege configuration
 CREATE DATABASE IF NOT EXISTS `typoglyph`;
 USE `typoglyph`;
 
+-- Limitations:
+--  Can't use NoSQL store as the host environment doesn't support it
+--  Can't use MySQL's JSON datatype as the host environment uses v5.5
+--
+-- Storing JSON in MySQL TEXT fields isn't exactly the prettiest solution but it's the easiest to
+-- implement. The backend can easily be modified in the future if a different storage format is
+-- required.
+-- 
+-- In this schema, the "id" field stored on the JSON graph must always match the SQL `id` field.
+--
 DROP TABLE IF EXISTS `puzzles`;
 CREATE TABLE `puzzles` (
---	Field name		Type		Allow null?	Default value				Is a PK?		Comment
-	`_id`			BIGINT		NOT NULL	AUTO_INCREMENT				PRIMARY KEY
-COMMENT 'Used to identify a given puzzle',
-	`_timestamp`	TIMESTAMP	NOT NULL	DEFAULT CURRENT_TIMESTAMP
-COMMENT 'When this puzzle was added',
-	`sentence`		TEXT		NOT NULL
-COMMENT 'Use a character wrapped in curly braces "{.}" anywhere in the sentence to represent a gap
-for the user to fill in. The character used will represent the correct answer for that gap. Just an
-open and close brace "{}" means the correct answer will be to leave the gap blank. Open/close
-braces and backslashes must be escaped with a preceding backslash if you want them to be
-interpreted literally. Note that some characters (esp. backslashes and quotes) may need to be
-further escaped when updating this field via SQL.',
-	`options`		TEXT		NOT NULL
-COMMENT 'The choices the user will be able to pick from, one after the other (only single character
-options are supported). Note: Backslashes and curly braces don\'t need to be escaped here (other
-than the normal escaping needed for SQL statements).' );
+	`id`   INT  NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Used to identify a given puzzle. Must always match the 'id' field on the JSON.",
+	`data` TEXT NOT NULL                            COMMENT "The puzzle stored in JSON format"
+);
--- env/mysql_generate_test_data.sql
+++ env/mysql_generate_test_data.sql
@@ -1,18 +1,319 @@
+-- Note: Backslashes must be escaped for both JSON and SQL. A single, literal backslash in a JSON
+--   string should be written as 4 consecutive backslashes. Other special JSON characters can be
+--   escaped with 2 backslashes before the special character.
+-- Note: Backslashes inside comments doesn't seem to be valid SQL.
 USE `typoglyph`;
 
 TRUNCATE `puzzles`;
-INSERT INTO `puzzles` (`options`, `sentence`) VALUES
-	-- Backslashes and single quotes must be escaped with a preceding backslash if you want them to appear literally in the table
-	('!',		'Hello{}world{!}'),
-	('.,?',		'This{}is{}a{}test{}sentence{.}Can{}you{}work{}out{}which{}punctuation{}to{}use{?}'),
-	('.,?',		'Hello{,}my{}name{}is{}Jake{.}How{}are{}you{}today{?}'),
-	(',.;-:',	'The{}dog{}ran{}over{}the{}car{,}but{}the{}driver{}was{}able{}to{}walk{}away{}uninjured{.}'),
-	('.,?',		'Have{}you{}tried{}turning{}it{}off{}and{}on{}again{?}'),
-	('.,?',		'Are{}you{}sure{}it\'s{}plugged{}in{?}'),
-	
-	
-	-- Specifically for testing edge cases
-	('',		'What{}if{}no{}punctuation{}is{}needed{}'),
-	('',		'What if there aren\'t even any gaps'),
-	('{}',		'{\\{}Example wrapped in curly brackets to ensure escaping works right{\\}}'),
-	('\\',		'{\\\\}Example wrapped in backslashes to ensure escaping works right{\\\\}');
+INSERT INTO `puzzles` (`id`, `data`) VALUES
+-- Hello world!
+(1, '{
+	"id": 1,
+	"sentenceFragments": [
+		{ "type": "Character", "value": "H" },
+		{ "type": "Character", "value": "e" },
+		{ "type": "Character", "value": "l" },
+		{ "type": "Character", "value": "l" },
+		{ "type": "Character", "value": "o" },
+		{ "type": "Gap", "solution": null },
+		{ "type": "Character", "value": "w" },
+		{ "type": "Character", "value": "o" },
+		{ "type": "Character", "value": "r" },
+		{ "type": "Character", "value": "l" },
+		{ "type": "Character", "value": "d" },
+		{ "type": "Gap", "solution": { "value": "!" }}
+	],
+	"options": [
+		{ "value": "." },
+		{ "value": "," },
+		{ "value": "!" },
+		{ "value": "?" }
+	]
+}'),
+
+-- Really exciting!!
+(2, '{
+	"id": 2,
+	"sentenceFragments": [
+		{ "type": "Character", "value": "R" },
+		{ "type": "Character", "value": "e" },
+		{ "type": "Character", "value": "a" },
+		{ "type": "Character", "value": "l" },
+		{ "type": "Character", "value": "l" },
+		{ "type": "Character", "value": "y" },
+		{ "type": "Gap", "solution": null },
+		{ "type": "Character", "value": "e" },
+		{ "type": "Character", "value": "x" },
+		{ "type": "Character", "value": "c" },
+		{ "type": "Character", "value": "i" },
+		{ "type": "Character", "value": "t" },
+		{ "type": "Character", "value": "i" },
+		{ "type": "Character", "value": "n" },
+		{ "type": "Character", "value": "g" },
+		{ "type": "Gap", "solution": { "value": "!!" }}
+	],
+	"options": [
+		{ "value": "!!" }
+	]
+}'),
+
+-- Hello, my name is Jake. How are you today?
+(3, '{
+	"id": 3,
+	"sentenceFragments": [
+		{ "type": "Character", "value": "H" },
+		{ "type": "Character", "value": "e" },
+		{ "type": "Character", "value": "l" },
+		{ "type": "Character", "value": "l" },
+		{ "type": "Character", "value": "o" },
+		{ "type": "Gap", "solution": { "value": "," }},
+		{ "type": "Character", "value": "m" },
+		{ "type": "Character", "value": "y" },
+		{ "type": "Gap", "solution": null },
+		{ "type": "Character", "value": "n" },
+		{ "type": "Character", "value": "a" },
+		{ "type": "Character", "value": "m" },
+		{ "type": "Character", "value": "e" },
+		{ "type": "Gap", "solution": null },
+		{ "type": "Character", "value": "i" },
+		{ "type": "Character", "value": "s" },
+		{ "type": "Gap", "solution": null },
+		{ "type": "Character", "value": "J" },
+		{ "type": "Character", "value": "a" },
+		{ "type": "Character", "value": "k" },
+		{ "type": "Character", "value": "e" },
+		{ "type": "Gap", "solution": { "value": "." }},
+		{ "type": "Character", "value": "H" },
+		{ "type": "Character", "value": "o" },
+		{ "type": "Character", "value": "w" },
+		{ "type": "Gap", "solution": null },
+		{ "type": "Character", "value": "a" },
+		{ "type": "Character", "value": "r" },
+		{ "type": "Character", "value": "e" },
+		{ "type": "Gap", "solution": null },
+		{ "type": "Character", "value": "y" },
+		{ "type": "Character", "value": "o" },
+		{ "type": "Character", "value": "u" },
+		{ "type": "Gap", "solution": null },
+		{ "type": "Character", "value": "t" },
+		{ "type": "Character", "value": "o" },
+		{ "type": "Character", "value": "d" },
+		{ "type": "Character", "value": "a" },
+		{ "type": "Character", "value": "y" },
+		{ "type": "Gap", "solution": { "value": "?" }}
+	],
+	"options": [
+		{ "value": "." },
+		{ "value": "," },
+		{ "value": "!" },
+		{ "value": "?" }
+	]
+}'),
+
+-- The dog ran over the car, but the driver was able to walk away uninjured.
+(4, '{
+	"id": 4,
+	"sentenceFragments": [
+		{ "type": "Character", "value": "T" },
+		{ "type": "Character", "value": "h" },
+		{ "type": "Character", "value": "e" },
+		{ "type": "Gap", "solution": null },
+		{ "type": "Character", "value": "d" },
+		{ "type": "Character", "value": "o" },
+		{ "type": "Character", "value": "g" },
+		{ "type": "Gap", "solution": null },
+		{ "type": "Character", "value": "r" },
+		{ "type": "Character", "value": "a" },
+		{ "type": "Character", "value": "n" },
+		{ "type": "Gap", "solution": null },
+		{ "type": "Character", "value": "o" },
+		{ "type": "Character", "value": "v" },
+		{ "type": "Character", "value": "e" },
+		{ "type": "Character", "value": "r" },
+		{ "type": "Gap", "solution": null },
+		{ "type": "Character", "value": "t" },
+		{ "type": "Character", "value": "h" },
+		{ "type": "Character", "value": "e" },
+		{ "type": "Gap", "solution": null },
+		{ "type": "Character", "value": "c" },
+		{ "type": "Character", "value": "a" },
+		{ "type": "Character", "value": "r" },
+		{ "type": "Gap", "solution": { "value": "," }},
+		{ "type": "Character", "value": "b" },
+		{ "type": "Character", "value": "u" },
+		{ "type": "Character", "value": "t" },
+		{ "type": "Gap", "solution": null },
+		{ "type": "Character", "value": "t" },
+		{ "type": "Character", "value": "h" },
+		{ "type": "Character", "value": "e" },
+		{ "type": "Gap", "solution": null },
+		{ "type": "Character", "value": "d" },
+		{ "type": "Character", "value": "r" },
+		{ "type": "Character", "value": "i" },
+		{ "type": "Character", "value": "v" },
+		{ "type": "Character", "value": "e" },
+		{ "type": "Character", "value": "r" },
+		{ "type": "Gap", "solution": null },
+		{ "type": "Character", "value": "w" },
+		{ "type": "Character", "value": "a" },
+		{ "type": "Character", "value": "s" },
+		{ "type": "Gap", "solution": null },
+		{ "type": "Character", "value": "a" },
+		{ "type": "Character", "value": "b" },
+		{ "type": "Character", "value": "l" },
+		{ "type": "Character", "value": "e" },
+		{ "type": "Gap", "solution": null },
+		{ "type": "Character", "value": "t" },
+		{ "type": "Character", "value": "o" },
+		{ "type": "Gap", "solution": null },
+		{ "type": "Character", "value": "w" },
+		{ "type": "Character", "value": "a" },
+		{ "type": "Character", "value": "l" },
+		{ "type": "Character", "value": "k" },
+		{ "type": "Gap", "solution": null },
+		{ "type": "Character", "value": "a" },
+		{ "type": "Character", "value": "w" },
+		{ "type": "Character", "value": "a" },
+		{ "type": "Character", "value": "y" },
+		{ "type": "Gap", "solution": null },
+		{ "type": "Character", "value": "u" },
+		{ "type": "Character", "value": "n" },
+		{ "type": "Character", "value": "i" },
+		{ "type": "Character", "value": "n" },
+		{ "type": "Character", "value": "j" },
+		{ "type": "Character", "value": "u" },
+		{ "type": "Character", "value": "r" },
+		{ "type": "Character", "value": "e" },
+		{ "type": "Character", "value": "d" },
+		{ "type": "Gap", "solution": { "value": "." }}
+	],
+	"options": [
+		{ "value": "." },
+		{ "value": "," },
+		{ "value": "!" },
+		{ "value": "?" }
+	]
+}'),
+
+-- No punctuation required
+(5, '{
+	"id": 5,
+	"sentenceFragments": [
+		{ "type": "Character", "value": "N" },
+		{ "type": "Character", "value": "o" },
+		{ "type": "Gap", "solution": null },
+		{ "type": "Character", "value": "p" },
+		{ "type": "Character", "value": "u" },
+		{ "type": "Character", "value": "n" },
+		{ "type": "Character", "value": "c" },
+		{ "type": "Character", "value": "t" },
+		{ "type": "Character", "value": "u" },
+		{ "type": "Character", "value": "a" },
+		{ "type": "Character", "value": "t" },
+		{ "type": "Character", "value": "i" },
+		{ "type": "Character", "value": "o" },
+		{ "type": "Character", "value": "n" },
+		{ "type": "Gap", "solution": null },
+		{ "type": "Character", "value": "r" },
+		{ "type": "Character", "value": "e" },
+		{ "type": "Character", "value": "q" },
+		{ "type": "Character", "value": "u" },
+		{ "type": "Character", "value": "i" },
+		{ "type": "Character", "value": "r" },
+		{ "type": "Character", "value": "e" },
+		{ "type": "Character", "value": "d" }
+	],
+	"options": []
+}'),
+
+-- No gaps to fill here
+(6, '{
+	"id": 6,
+	"sentenceFragments": [
+		{ "type": "Character", "value": "N" },
+		{ "type": "Character", "value": "o" },
+		{ "type": "Character", "value": " " },
+		{ "type": "Character", "value": "g" },
+		{ "type": "Character", "value": "a" },
+		{ "type": "Character", "value": "p" },
+		{ "type": "Character", "value": "s" },
+		{ "type": "Character", "value": " " },
+		{ "type": "Character", "value": "t" },
+		{ "type": "Character", "value": "o" },
+		{ "type": "Character", "value": " " },
+		{ "type": "Character", "value": "f" },
+		{ "type": "Character", "value": "i" },
+		{ "type": "Character", "value": "l" },
+		{ "type": "Character", "value": "l" },
+		{ "type": "Character", "value": " " },
+		{ "type": "Character", "value": "h" },
+		{ "type": "Character", "value": "e" },
+		{ "type": "Character", "value": "r" },
+		{ "type": "Character", "value": "e" }
+	],
+	"options": []
+}'),
+
+-- {Wrapped in braces}
+(7, '{
+	"id": 7,
+	"sentenceFragments": [
+		{ "type": "Gap", "solution": { "value": "{" }},
+		{ "type": "Character", "value": "W" },
+		{ "type": "Character", "value": "r" },
+		{ "type": "Character", "value": "a" },
+		{ "type": "Character", "value": "p" },
+		{ "type": "Character", "value": "p" },
+		{ "type": "Character", "value": "e" },
+		{ "type": "Character", "value": "d" },
+		{ "type": "Character", "value": " " },
+		{ "type": "Character", "value": "i" },
+		{ "type": "Character", "value": "n" },
+		{ "type": "Character", "value": " " },
+		{ "type": "Character", "value": "b" },
+		{ "type": "Character", "value": "r" },
+		{ "type": "Character", "value": "a" },
+		{ "type": "Character", "value": "c" },
+		{ "type": "Character", "value": "e" },
+		{ "type": "Character", "value": "s" },
+		{ "type": "Gap", "solution": { "value": "}" }}
+	],
+	"options": [
+		{ "value": "{" },
+		{ "value": "}" }
+	]
+}'),
+
+-- /Wrapped in backslashes/
+(8, '{
+	"id": 8,
+	"sentenceFragments": [
+		{ "type": "Gap", "solution": { "value": "\\\\" }},
+		{ "type": "Character", "value": "W" },
+		{ "type": "Character", "value": "r" },
+		{ "type": "Character", "value": "a" },
+		{ "type": "Character", "value": "p" },
+		{ "type": "Character", "value": "p" },
+		{ "type": "Character", "value": "e" },
+		{ "type": "Character", "value": "d" },
+		{ "type": "Character", "value": " " },
+		{ "type": "Character", "value": "i" },
+		{ "type": "Character", "value": "n" },
+		{ "type": "Character", "value": " " },
+		{ "type": "Character", "value": "b" },
+		{ "type": "Character", "value": "a" },
+		{ "type": "Character", "value": "c" },
+		{ "type": "Character", "value": "k" },
+		{ "type": "Character", "value": "s" },
+		{ "type": "Character", "value": "l" },
+		{ "type": "Character", "value": "a" },
+		{ "type": "Character", "value": "s" },
+		{ "type": "Character", "value": "h" },
+		{ "type": "Character", "value": "e" },
+		{ "type": "Character", "value": "s" },
+		{ "type": "Gap", "solution": { "value": "\\\\" }}
+	],
+	"options": [
+		{ "value": "\\\\" }
+	]
+}');
