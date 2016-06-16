0aa0caf4948025cb23ccc7ff923cf9af536ad159
jakemarsden <jakemarsdenjm@gmail.com>
2016-06-16 15:15:32 +1200

    Removed build script in favour of Gulp so I can use Linux for dev work

--- src/scripts/requireConfig.js
+++ src/scripts/requireConfig.js
@@ -4,19 +4,23 @@ require.config({
 		// Entry points
 		admin: "../admin",
 		index: "../index",
-		
+
 		// Libraries
 		howler: "../lib/howler-v1.1.29/howler.min",
 		interact: "../lib/interact-v1.2.6/interact.min"
 	}
 });
 
-// See https://github.com/requirejs/requirejs/issues/354#issuecomment-44496216
-// See http://stackoverflow.com/a/22745553/14731
-var scripts = document.getElementsByTagName("script");
-var thisScriptTag = scripts[scripts.length - 1];
-var dataMain = thisScriptTag.getAttribute("data-main");
+// horrible hack to skip this when parsing the config as part of the build
+// see gulpfile.js
+if (typeof window !== "undefined") {
+	// See https://github.com/requirejs/requirejs/issues/354#issuecomment-44496216
+	// See http://stackoverflow.com/a/22745553/14731
+	var scripts = document.getElementsByTagName("script");
+	var thisScriptTag = scripts[scripts.length - 1];
+	var dataMain = thisScriptTag.getAttribute("data-main");
 
-if (dataMain) {
-    require([dataMain]);
+	if (dataMain) {
+		require([dataMain]);
+	}
 }
